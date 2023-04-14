325c1d32ea766b5326e50d45dbb8c0bc532abbb7
Harshul Vig <130694488+twerk4hv@users.noreply.github.com>
2023-04-14 12:11:52 +0530

    Delete JS Codes - 7.js

--- JS Codes - 7.js
+++
@@ -1,9 +0,0 @@
-line = prompt("write a sentence");
-words = line.split(" ");
-
-for (let i = 0; i < words.length; i++) {
-    words[i] = words[i][0].toUpperCase() + words[i].substr(1);
-}
-
-words.join(" ");
-console.log(line)
